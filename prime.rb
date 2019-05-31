f75513cb9be64e1e747f1ceb985aece7d875e95c
Alicia Fasciocco <afasciocco@gmail.com>
2019-05-31 22:05:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,7 @@
 # Add  code here!
-require 'cmath'
-
 def prime?(num)
   if num < 0 || num == 0 || num == 1 
     false
-  (2..CMath.sqrt(num)).each {|i| return false if num % i == 0}
+  else (2..Math.sqrt(num)).each {|i| return false if num % i == 0}
   true
 end
